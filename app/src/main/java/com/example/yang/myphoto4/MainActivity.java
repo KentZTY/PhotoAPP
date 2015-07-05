d861a7f834d71d06eefa04785bb5efc227ae7289
y8877322 <yxyshef@live.cn>
2015-07-06 00:02:48 +0100

    Camera Function added

--- app/src/main/java/com/example/yang/myphoto4/MainActivity.java
+++ app/src/main/java/com/example/yang/myphoto4/MainActivity.java
@@ -5,28 +5,42 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity
 {
     private static final int SELECT_PICTURE = 1;
+    private static final int REQUEST_CAPTURE_CAMERA = 1;
     private String selectedImagePath1;
-    private ImageView img;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img = (ImageView)findViewById(R.id.imageView01);
 
         (findViewById(R.id.button01))
                 .setOnClickListener(new View.OnClickListener() {
                     public void onClick(View arg0) {
                         Intent intent = new Intent(Intent.ACTION_PICK);
                         intent.setType("image/*");
-                        startActivityForResult(Intent.createChooser(intent,"Select Picture"), SELECT_PICTURE);
+                        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+                    }
+                });
+        (findViewById(R.id.button02))
+                .setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View arg0) {
+                        String state = Environment.getExternalStorageState();
+                        if (state.equals(Environment.MEDIA_MOUNTED)) {
+                            Intent getImageByCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(getImageByCamera, REQUEST_CAPTURE_CAMERA);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Make sure you've insert SD card.", Toast.LENGTH_LONG).show();
+                        }
                     }
                 });
     }
@@ -37,7 +51,6 @@ public class MainActivity extends Activity
                 Uri selectedImageUri = data.getData();
                 selectedImagePath1 = getPath(selectedImageUri);
                 System.out.println("Image Path : " + selectedImagePath1);
-                img.setImageURI(selectedImageUri);
                 Intent intent1 = new Intent();
                 intent1.setClass(MainActivity.this, DisplayImageActivity.class);
                 intent1.setData(selectedImageUri);
