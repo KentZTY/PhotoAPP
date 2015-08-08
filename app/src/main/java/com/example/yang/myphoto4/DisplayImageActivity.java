d8dea48e7a5a6dbea4dc44ec54b80006eac56917
y8877322 <yxyshef@live.cn>
2015-08-08 01:15:56 +0100

    Sticker Border modification (BETA 0.5)

--- app/src/main/java/com/example/yang/myphoto4/DisplayImageActivity.java
+++ app/src/main/java/com/example/yang/myphoto4/DisplayImageActivity.java
@@ -44,9 +44,9 @@ public class DisplayImageActivity extends Activity{
     private ImageView borderImage;
     private static final int sticker = 1;
     private static final int border = 2;
-    private static final int NONE = 3;// 初始状态
-    private static final int DRAG = 4;// 拖动
-    private static final int ZOOM_OR_ROTATE = 5; // 缩放或旋转
+    private static final int NONE = 3;
+    private static final int DRAG = 4;
+    private static final int ZOOM_OR_ROTATE = 5;
     private static final int DELETE = 6;
     int mode = NONE;
 
@@ -411,16 +411,9 @@ public class DisplayImageActivity extends Activity{
         public boolean onTouch(View v, MotionEvent event) {
 
             switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                // 主点按下
                 case MotionEvent.ACTION_DOWN:
                     modeChooser(v, event);
                     break;
-                // 副点按下
-                case MotionEvent.ACTION_POINTER_DOWN:
-                    if (event.getActionIndex() > 1)
-                        break;
-                     //multiPoint(v, event);
-                     //break;
                 case MotionEvent.ACTION_UP:
                     if(mode == DELETE){
                     deleteSticker((myImageView)v);
@@ -431,11 +424,9 @@ public class DisplayImageActivity extends Activity{
                     mode = NONE;
                     break;
                 case MotionEvent.ACTION_MOVE:
-
-                    if (mode == ZOOM_OR_ROTATE) {// 进行 放大缩小
+                    if (mode == ZOOM_OR_ROTATE) {
                         zoomAndRotate(v, event);
                     }
-
                     if (mode == DRAG) {
                         drag(v, event);
                     }
@@ -464,7 +455,6 @@ public class DisplayImageActivity extends Activity{
                 currentImage = (myImageView)v;
             }
             ((myImageView)v).pA.set(event.getX() + ((myImageView)v).viewL, event.getY() + ((myImageView)v).viewT);
-            // pB.set(event.getX(), event.getY());
             if (((myImageView)v).isactiondownicon((int) event.getX(), (int) event.getY()) == 2) {
                 mode = ZOOM_OR_ROTATE;
             }
@@ -480,8 +470,6 @@ public class DisplayImageActivity extends Activity{
 
     private void zoomAndRotate(View v, MotionEvent event){
         float sf = 1f;
-        // float newx=event.getX();
-        // float newy=event.getY();
         ((myImageView)v).pB.set(event.getX() + ((myImageView)v).viewL, event.getY() + ((myImageView)v).viewT);
         float realL = (float) Math.sqrt((float) (((myImageView)v).mBitmap.getWidth()
                 * ((myImageView)v).mBitmap.getWidth() + ((myImageView)v).mBitmap.getHeight()
@@ -489,55 +477,49 @@ public class DisplayImageActivity extends Activity{
         float newL = (float) Math.sqrt((((myImageView)v).pB.x - (float) ((myImageView)v).cpoint.x)
                 * (((myImageView)v).pB.x - (float) ((myImageView)v).cpoint.x) + (((myImageView)v).pB.y - (float) ((myImageView)v).cpoint.y)
                 * (((myImageView)v).pB.y - (float) ((myImageView)v).cpoint.y));
-        // System.out.println("r,n:"+realL+"|"+newL);
+
         sf = newL / realL;
-        // 角度
         double a = ((myImageView)v).spacing(((myImageView)v).pA.x, ((myImageView)v).pA.y, (float) ((myImageView)v).cpoint.x,
                 (float) ((myImageView)v).cpoint.y);
         double b = ((myImageView)v).spacing(((myImageView)v).pB.x, ((myImageView)v).pB.y, ((myImageView)v).pA.x, ((myImageView)v).pA.y);
         double c = ((myImageView)v).spacing(((myImageView)v).pB.x, ((myImageView)v).pB.y, (float) ((myImageView)v).cpoint.x,
                 (float) ((myImageView)v).cpoint.y);
         double cosB = (a * a + c * c - b * b) / (2 * a * c);
-        if (cosB > 1) {// 浮点运算的时候 cosB 有可能大于1.
-            System.out.println(" sf:" + sf + " cosB:" + cosB);
+        if (cosB > 1) {
             cosB = 1f;
         }
         double angleB = Math.acos(cosB);
         float newAngle = (float) (angleB / Math.PI * 180);
-        // newAngle=0f;
 
         float p1x = ((myImageView)v).pA.x - (float) ((myImageView)v).cpoint.x;
         float p2x = ((myImageView)v).pB.x - (float) ((myImageView)v).cpoint.x;
-
         float p1y = ((myImageView)v).pA.y - (float) ((myImageView)v).cpoint.y;
         float p2y = ((myImageView)v).pB.y - (float) ((myImageView)v).cpoint.y;
-        // 正反向。
+
         if (p1x == 0) {
-            if (p2x > 0 && p1y >= 0 && p2y >= 0) {// 由 第4-》第3
+            if (p2x > 0 && p1y >= 0 && p2y >= 0) {
                 newAngle = -newAngle;
-            } else if (p2x < 0 && p1y < 0 && p2y < 0) {// 由 第2-》第1
+            } else if (p2x < 0 && p1y < 0 && p2y < 0) {
                 newAngle = -newAngle;
             }
         } else if (p2x == 0) {
-            if (p1x < 0 && p1y >= 0 && p2y >= 0) {// 由 第4-》第3
+            if (p1x < 0 && p1y >= 0 && p2y >= 0) {
                 newAngle = -newAngle;
-            } else if (p1x > 0 && p1y < 0 && p2y < 0) {// 由 第2-》第1
+            } else if (p1x > 0 && p1y < 0 && p2y < 0) {
                 newAngle = -newAngle;
             }
         } else if (p1x != 0 && p2x != 0 && p1y / p1x < p2y / p2x) {
-            if (p1x < 0 && p2x > 0 && p1y >= 0 && p2y >= 0) {// 由 第4-》第3
+            if (p1x < 0 && p2x > 0 && p1y >= 0 && p2y >= 0) {
                 newAngle = -newAngle;
-            } else if (p2x < 0 && p1x > 0 && p1y < 0 && p2y < 0) {// 由
-                // 第2-》第1
+            } else if (p2x < 0 && p1x > 0 && p1y < 0 && p2y < 0) {
                 newAngle = -newAngle;
             } else {
 
             }
         } else {
-            if (p2x < 0 && p1x > 0 && p1y >= 0 && p2y >= 0) {// 由 第3-》第4
+            if (p2x < 0 && p1x > 0 && p1y >= 0 && p2y >= 0) {
 
-            } else if (p2x > 0 && p1x < 0 && p1y < 0 && p2y < 0) {// 由
-                // 第1-》第2
+            } else if (p2x > 0 && p1x < 0 && p1y < 0 && p2y < 0) {
 
             } else {
                 newAngle = -newAngle;
@@ -554,28 +536,12 @@ public class DisplayImageActivity extends Activity{
 
     }
 
-    /*private void multiPoint(View v, MotionEvent event){
-        ((myImageView)v).dist = ((myImageView)v).spacing(event.getX(0), event.getY(0), event.getX(1),
-                event.getY(1));
-        //如果连续两点距离大于10，则判定为多点模式
-        if (((myImageView)v).dist > 10f) {
-            ((myImageView)v).savedMatrix.set(((myImageView)v).matrix);
-            ((myImageView)v).pA.set(event.getX(0), event.getY(0));
-            ((myImageView)v).pB.set(event.getX(1), event.getY(1));
-            ((myImageView)v).mid.set((event.getX(0) + event.getX(1)) / 2,
-                    (event.getY(0) + event.getY(1)) / 2);
-            mode = ZOOM_OR_ROTATE;
-        }
-    }*/
-
     private void drag(View v, MotionEvent event){
         ((myImageView)v).pB.set(event.getX() + ((myImageView)v).viewL, event.getY() + ((myImageView)v).viewT);
-        // 修改中心点
         ((myImageView)v).cpoint.x += ((myImageView)v).pB.x - ((myImageView)v).pA.x;
         ((myImageView)v).cpoint.y += ((myImageView)v).pB.y - ((myImageView)v).pA.y;
         ((myImageView)v).pA.x = ((myImageView)v).pB.x;
         ((myImageView)v).pA.y = ((myImageView)v).pB.y;
-        // this.setImageBitmap(this.mBitmap, cpoint, jd, sfxs);
         ((myImageView)v).setCPoint(((myImageView)v).cpoint);
 
     }
