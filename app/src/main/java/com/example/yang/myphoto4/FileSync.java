d5d287ce9ac5b922e6c6f80defc4e0beafd7a68e
reezhu <reezhuwy@gmail.com>
2015-08-31 20:44:08 +0100

    mirror changes

--- app/src/main/java/com/example/yang/myphoto4/FileSync.java
+++ app/src/main/java/com/example/yang/myphoto4/FileSync.java
@@ -80,6 +80,7 @@ public class FileSync extends Activity implements View.OnClickListener {
         //Environment.getExtemalStorageState()
         //Environment.MEDIA_MOUNTED
         //getAbsolutePath()
+
         if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
                 || !Environment.isExternalStorageRemovable()) {
             cachePath = context.getExternalCacheDir().getPath();
